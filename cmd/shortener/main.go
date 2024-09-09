0945762fd91b052e47520f9a94bd25955561913e
cbrn_wth_guitar <vladuha.vk@gmail.com>
2024-09-09 23:06:03 +0300

    rewrite app to Echo framework

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,57 +1,67 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"io"
 	"math/rand"
 	"net/http"
 )
 
-var Urls = map[string]string{}
+var (
+	Urls = map[string]string{}
+)
 
 func main() {
-	http.HandleFunc("/", urlShortener)
+	e := echo.New()
 
-	err := http.ListenAndServe(`:8080`, nil)
-	if err != nil {
-		panic(err)
-	}
+	// Add middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Create and return the group
+	g := e.Group("/")
+
+	// Define routes
+	g.POST("", CreateShortURL)
+	g.GET(":id", GetLongURL)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func urlShortener(res http.ResponseWriter, req *http.Request) {
-	switch req.Method {
-	case http.MethodPost:
-		// Handle POST request
-		req.URL.Scheme = "http"
-		shortURLPath := "/" + RandomString(6)
-		shortURL := req.URL.Scheme + "://" + req.Host + shortURLPath
+func CreateShortURL(c echo.Context) error {
+	// Handle POST request
+	c.Request().URL.Scheme = "http"
+	id := RandomString(6)
+	shortURL := c.Request().URL.Scheme + "://" + c.Request().Host + "/" + id
 
-		body, err := io.ReadAll(req.Body)
-		if err != nil {
-			http.Error(res, "No Url in Body", http.StatusBadRequest)
-		}
-		Urls[shortURLPath] = string(body)
-
-		// Writing Response
-		res.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		res.WriteHeader(http.StatusCreated)
-		io.WriteString(res, shortURL)
-
-	case http.MethodGet:
-		// Handle GET request
-		path := req.URL.Path
-
-		// Writing Response
-		_, state := Urls[path]
-		if !state {
-			http.Error(res, "Short URL not found", http.StatusNotFound)
-		}
-		res.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		res.Header().Set("Location", Urls[path])
-		res.WriteHeader(http.StatusTemporaryRedirect)
-
-	default:
-		http.Error(res, "Invalid request method", http.StatusMethodNotAllowed)
+	// Read body to create Map
+	body, err := io.ReadAll(c.Request().Body)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Read Body failed")
 	}
+	if string(body) == "" {
+		return c.String(http.StatusBadRequest, "Body is empty")
+	}
+	Urls[id] = string(body)
+
+	// Response writing
+	c.Response().Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	c.Response().WriteHeader(http.StatusCreated)
+
+	return c.String(http.StatusCreated, shortURL)
+}
+
+func GetLongURL(c echo.Context) error {
+	// Handle GET request
+	id := c.Param("id")
+	long, ok := Urls[id]
+	if !ok {
+		return c.String(http.StatusNotFound, "Short URL not found")
+	}
+	// Response writing
+	c.Response().Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	return c.Redirect(http.StatusTemporaryRedirect, long)
 }
 
 func RandomString(num int) string {
--- cmd/shortener/shortener__test.go
+++ cmd/shortener/shortener__test.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
 	"strings"
 	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestUrlShortener(t *testing.T) {
-	baseURL := "http://localhost:8080"
+func TestCreateShortURL(t *testing.T) {
+	baseURL := "http://localhost:8080/"
+
 	type want struct {
 		contentType  string
 		statusCode   int
 		responseBody string
 		location     string
 	}
+
 	tests := []struct {
 		name   string
 		method string
@@ -30,25 +34,35 @@ func TestUrlShortener(t *testing.T) {
 		{
 			name:   "POST request - create short URL",
 			method: http.MethodPost,
-			path:   baseURL + "/",
+			path:   baseURL,
 			body:   "https://example.com",
 			urls:   map[string]string{},
 			want: want{
-				contentType:  "text/plain; charset=utf-8",
+				contentType:  "text/plain; charset=UTF-8",
 				statusCode:   http.StatusCreated,
-				responseBody: baseURL + "/" + "\\w{6}",
+				responseBody: baseURL + "\\w{6}",
+			},
+		},
+		{
+			name:   "POST request - empty body",
+			method: http.MethodPost,
+			path:   baseURL,
+			body:   "",
+			urls:   map[string]string{},
+			want: want{
+				contentType:  "text/plain; charset=UTF-8",
+				statusCode:   http.StatusBadRequest,
+				responseBody: "Body is empty",
 			},
 		},
 		{
 			name:   "GET request - valid short URL",
 			method: http.MethodGet,
-			path:   baseURL + "/abc123",
+			path:   "/abc123",
 			body:   "",
-			urls: map[string]string{
-				"/abc123": "https://example.com",
-			},
+			urls:   map[string]string{"abc123": "https://example.com"},
 			want: want{
-				contentType: "text/plain; charset=utf-8",
+				contentType: "text/plain; charset=UTF-8",
 				statusCode:  http.StatusTemporaryRedirect,
 				location:    "https://example.com",
 			},
@@ -56,37 +70,52 @@ func TestUrlShortener(t *testing.T) {
 		{
 			name:   "GET request - invalid short URL",
 			method: http.MethodGet,
-			path:   baseURL + "/invalid",
+			path:   baseURL + "invalid",
 			body:   "",
 			urls:   map[string]string{},
 			want: want{
-				contentType:  "text/plain; charset=utf-8",
+				contentType:  "text/plain; charset=UTF-8",
 				statusCode:   http.StatusNotFound,
 				responseBody: "Short URL not found",
 			},
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			e := echo.New()
 			Urls = tt.urls
+
 			request := httptest.NewRequest(tt.method, tt.path, strings.NewReader(tt.body))
 			w := httptest.NewRecorder()
-			urlShortener(w, request)
+			c := e.NewContext(request, w)
+
+			var err error
+			if tt.method == http.MethodPost {
+				err = CreateShortURL(c)
+			} else {
+				c.SetParamNames("id")
+				c.SetParamValues(tt.path[1:])
+
+				err = GetLongURL(c)
+			}
+			require.NoError(t, err)
 
 			result := w.Result()
+			defer result.Body.Close()
 
 			assert.Equal(t, tt.want.statusCode, result.StatusCode)
 			assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
 
-			urlResult, err := io.ReadAll(result.Body)
-			require.NoError(t, err)
-			err = result.Body.Close()
-			require.NoError(t, err)
+			if tt.want.location != "" {
+				assert.Equal(t, tt.want.location, result.Header.Get("Location"))
+			}
 
 			if tt.want.responseBody != "" {
-				assert.Regexp(t, regexp.MustCompile(tt.want.responseBody), string(urlResult))
+				bodyContent, err := io.ReadAll(result.Body)
+				require.NoError(t, err)
+				assert.Regexp(t, regexp.MustCompile(tt.want.responseBody), string(bodyContent))
 			}
 		})
-
 	}
 }
